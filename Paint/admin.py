0ea90e686e88f438b1d4ac066ae0bc3b371fec20
ziho <hzhhzh1999@gmail.com>
2022-01-12 21:04:56 +0800

    First Draft

--- Paint/admin.py
+++ Paint/admin.py
@@ -3,12 +3,16 @@
 # password: psypaint
 
 from django.contrib import admin
-from .models import testersData
+from Paint.models import testersData
 
 
 # Register your models here.
-@admin.register(testersData)
-class testersDateAdmin(admin.ModelAdmin):
-    list_display = (
-        'tester_Name', 'tester_Program', 'tester_Age', 'tester_Gender', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8',
-        'q9', 'q10', 'file_url', 'generate_time')
+# @admin.register(testersData)
+# class testersDateAdmin(admin.ModelAdmin):
+#     list_display = (
+#         'tester_Name', 'tester_Program', 'tester_Age', 'tester_Gender', 'q1', 'q2', 'q3', 'q4', 'q5', 'q6', 'q7', 'q8',
+#         'q9', 'q10', 'file_url', 'generate_time')
+
+
+# Register your models here.
+admin.site.register(testersData)
